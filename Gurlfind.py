c9c223d683bf8149ce354d37de3c2fe3b28a09e0
aman chauhan <124485947+cyberxaman@users.noreply.github.com>
2023-02-23 23:14:50 +0530

    Update Gurlfind.py

--- Gurlfind.py
+++ Gurlfind.py
@@ -1,23 +1,15 @@
 import os
-import argparse
 
-try:
-    from googlesearch import search
-except ImportError:
-    print("Error: the 'googlesearch' module is not installed. Please install it and try again.")
-    quit()
+# clear the console screen
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
 
-def clear_screen():
-    """Clears the console screen based on OS type"""
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-
-def print_banner():
-    """Prints the banner message"""
+def banner():
     print("\033[32;1m")
     print("""
+
   ________             .__   _____.__            .___
  /  _____/ __ _________|  |_/ ____\__| ____    __| _/
 /   \  ___|  |  \_  __ \  |\   __\|  |/    \  / __ | 
@@ -29,35 +21,18 @@ def print_banner():
 """)
     print("\033[33;1m MADE BY CYBERXEAL")
 
-def search_query(query, num_results):
-    """Searches Google for the given query and returns the top 'num_results' results"""
-    results = []
-    for result in search(query, tld='com', lang='en', num=num_results, start=0, stop=None, pause=2.0):
-        results.append(result)
-    return results
+banner()
 
-def save_results(results, output_file):
-    """Saves the search results to the specified output file"""
-    with open(output_file, 'w') as file:
-        for result in results:
-            file.write(result + '\n')
+qu = input("\nEnter your query: ")
 
-def main():
-    parser = argparse.ArgumentParser(description='Search Google for the given query and save the results to a file.')
-    parser.add_argument('-u', '--query', type=str, required=True, help='The query to search for.')
-    parser.add_argument('-o', '--output', type=str, required=True, help='The output file to save the results to.')
-    parser.add_argument('-n', '--num_results', type=int, default=10, help='The number of search results to retrieve (default: 10).')
-    args = parser.parse_args()
+try:
+    from googlesearch import search
+except ImportError:
+    print("Error: the 'googlesearch' module is not installed. Please install it and try again.")
+    quit()
+ 
+# to search
+query = qu
 
-    clear_screen()
-    print_banner()
-
-    query = args.query
-    num_results = args.num_results
-    output_file = args.output
-
-    results = search_query(query, num_results)
-    save_results(results, output_file)
-
-if __name__ == '__main__':
-    main()
+for j in search(query, tld='com', lang='en', num=10, start=0, stop=None, pause=2.0):
+    print(j)
